ca05a11bd0f01ddb6a1d390399431f4ff62d2130
Rego <regopadilla1204@gmail.com>
2021-10-05 00:56:06 +0800

    changed notif msg

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -74,7 +74,7 @@ function ContactForm() {
     notification = {
       status: 'success',
       title: 'Success!',
-      message: 'Message sent successfully!',
+      message: 'Message sent successfully!!!!',
     };
   }
 
